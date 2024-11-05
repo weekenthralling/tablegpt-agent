9216da6cde67511d479367607a35c55b2d4eb091
Mo Zhou <zc@zjuici.com>
2024-11-05 18:54:55 +0800

    feat: add agent-eval

---
+++ eval/agent_eval/grader/output_parser.py
@@ -0,0 +1,53 @@
+from typing import Any
+
+from langchain.evaluation.criteria.eval_chain import Criteria
+from langchain.evaluation.scoring.eval_chain import (
+    _FIND_DOUBLE_BRACKETS,
+    ScoreStringResultOutputParser,
+)
+
+DEFAULT_CRITERIA = [
+    Criteria.HELPFULNESS,
+    Criteria.RELEVANCE,
+    Criteria.CORRECTNESS,
+    Criteria.DEPTH,
+    Criteria.DETAIL,
+]
+
+
+class FloatScoreOutputParser(ScoreStringResultOutputParser):
+    prefix: str = "Score:"  # Or maybe `None`?
+    lower_bound: float = 0.0
+    upper_bound: float = 1.0
+
+    def parse(self, text: str) -> dict[str, Any]:
+        """Parse the output text.
+
+        Args:
+            text (str): The output text to parse.
+
+        Returns:
+            dict: The parsed output.
+
+        Raises:
+            ValueError: If the verdict is invalid.
+        """
+        match = _FIND_DOUBLE_BRACKETS.search(text)
+
+        if match:
+            score_str = match.group(1).strip()
+            score = float(score_str)
+            if score > self.upper_bound or score < self.lower_bound:
+                raise ValueError(
+                    f"Invalid output: {text}. "
+                    f"Output must contain a double bracketed string with the verdict between {self.lower_bound} and {self.upper_bound}."
+                )
+            reason = text.rsplit(self.prefix, maxsplit=1)[0].strip()
+            return {
+                "reason": reason,
+                "score": round(score, 2),
+            }
+        else:
+            raise ValueError(
+                f"Invalid output: {text}. Output must contain a double bracketed string. example: [[0.5]]"
+            )
---
+++ eval/agent_eval/grader/prompt.py
@@ -0,0 +1,47 @@
+INSTRUCTION = """You are a teacher grading a quiz. Start by providing a brief reason for the rating you will assign. Then, assign a rating on a scale from 0.0 to 1.0, using the format: "Score: [[score]]" (e.g., "Score: [[0.5]]").
+{criteria}
+{redlines}
+## Quiz
+Question: {question}
+{reference_answer}
+Answer: {answer}
+"""
+
+
+DEFAULT_CRITERIA_WITH_REFERENCE_ANSWER = [
+    "Grade the student answers based ONLY on their factual accuracy relative to the ground truth answer.",
+    "Ensure that the student answer does not contain any conflicting statements.",
+    "It is OK if the student answer contains more information than the ground truth answer, as long as it is factually accurate relative to the ground truth answer.",
+]
+
+
+# Picked from `langchain.evaluation.criteria.eval_chain.Criteria`
+DEFAULT_CRITERIA_WITHOUT_REFERENCE_ANSWER = [
+    "Is the submission correct, accurate, and factual?",
+    "Is the submission concise and to the point?",
+    "Is the submission helpful, insightful, and appropriate?",
+]
+
+
+def format_criteria(criteria: list[str]) -> str:
+    if not criteria:
+        return ""
+    return f"""## Evaluation Criteria
+Consider the following criteria when assigning the rating:
+{"\n".join(map(lambda x: "- " + x, criteria))}
+"""
+
+
+def format_redlines(attentions: list[str]) -> str:
+    if not attentions:
+        return ""
+    return f"""## Redlines
+If the answer touches one of the redlines listed below, assign a score of [[0.0]] directly.
+{"\n".join(map(lambda x: "- " + x, attentions))}
+"""
+
+
+def format_reference_answer(reference_answer: str) -> str:
+    if not reference_answer:
+        return ""
+    return f"Reference Answer: {reference_answer}"
