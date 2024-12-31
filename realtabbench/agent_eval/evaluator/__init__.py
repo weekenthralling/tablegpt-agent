ca28f7229401f9537f7f9a2fb45fbbae0e2a5545
Junlin Zhou <jlzhou@zjuici.com>
2024-12-31 02:56:24 +0000

    chore: simplify evaluator chain

--- realtabbench/agent_eval/evaluator/__init__.py
+++ realtabbench/agent_eval/evaluator/__init__.py
@@ -2,7 +2,6 @@ from operator import itemgetter
 
 from langchain_core.language_models import BaseLanguageModel
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnableParallel
 
 from .output_parser import FloatScoreOutputParser
 from .prompt import (
@@ -14,17 +13,17 @@ from .prompt import (
 
 PROMPT = ChatPromptTemplate.from_messages([("user", INSTRUCTION)])
 
-output_parser = FloatScoreOutputParser()
-
-
-formatter = RunnableParallel(
-    criteria=lambda x: (format_criteria(x["criteria"]) if x.get("criteria") else ""),
-    redlines=lambda x: (format_redlines(x["redlines"]) if x.get("redlines") else ""),
-    reference_answer=lambda x: (format_reference_answer(x["reference_answer"]) if x.get("reference_answer") else ""),
-    question=itemgetter("question"),
-    answer=itemgetter("answer"),
-)
-
 
 def create_evaluator_runnable(llm: BaseLanguageModel):
-    return formatter | PROMPT | llm | output_parser
+    return (
+        {
+            "criteria": lambda x: (format_criteria(x["criteria"]) if x.get("criteria") else ""),
+            "redlines": lambda x: (format_redlines(x["redlines"]) if x.get("redlines") else ""),
+            "reference_answer": lambda x: (format_reference_answer(x["reference_answer"]) if x.get("reference_answer") else ""),
+            "question": itemgetter("question"),
+            "answer": itemgetter("answer"),
+        }
+        | PROMPT
+        | llm
+        | FloatScoreOutputParser
+    )
